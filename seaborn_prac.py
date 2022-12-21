44e0fa26c41d9b8eb08c85d9865feb9e89bedfde
unknown <soojlee0106@naver.com>
2022-12-21 14:40:25 +0900

    Finish seaborn

--- seaborn_prac.py
+++ seaborn_prac.py
@@ -1,34 +1,60 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-df = sns.load_dataset("penguins")
-print(df["species"].unique())
-print(df["island"].unique())
+# Penguin
+# df = sns.load_dataset("penguins")
+# print(df["species"].unique())
+# print(df["island"].unique())
 
-print(df.groupby("species")["island"].value_counts())
+# print(df.groupby("species")["island"].value_counts())
+
+# # sns.scatterplot(data=df, x="flipper_length_mm",
+# #                 y="bill_length_mm", hue="species")
+
+# # sns.histplot(data=df, x="species", hue="species")
+
+# fig, axes = plt.subplots(2, 2)
+# sns.scatterplot(data=df, x="flipper_length_mm",
+#                 y="bill_length_mm", hue="species", ax=axes[0, 0])
 
 # sns.scatterplot(data=df, x="flipper_length_mm",
-#                 y="bill_length_mm", hue="species")
+#                 y="bill_length_mm", hue="species", ax=axes[0, 1])
 
-# sns.histplot(data=df, x="species", hue="species")
+# sns.histplot(data=df, x="species", hue="species", ax=axes[1, 0])
+# sns.histplot(data=df, x="species", hue="species", ax=axes[1, 1])
 
-fig, axes = plt.subplots(2, 2)
-sns.scatterplot(data=df, x="flipper_length_mm",
-                y="bill_length_mm", hue="species", ax=axes[0, 0])
+# sns.displot(data=df, x="flipper_length_mm",
+#             kind="hist", hue="species", col="species", height=3, aspect=1)
 
-sns.scatterplot(data=df, x="flipper_length_mm",
-                y="bill_length_mm", hue="species", ax=axes[0, 1])
+# sns.displot(data=df, x="flipper_length_mm",
+#             kind="kde", hue="species", col="species", height=3, aspect=1)
 
-sns.histplot(data=df, x="species", hue="species", ax=axes[1, 0])
-sns.histplot(data=df, x="species", hue="species", ax=axes[1, 1])
+# sns.relplot(data=df, x="flipper_length_mm", y="bill_length_mm",
+#             kind="scatter", hue="species", col="species", height=3, aspect=1)
 
-sns.displot(data=df, x="flipper_length_mm",
-            kind="hist", hue="species", col="species", height=3, aspect=1)
+# Titanic
+# df = pd.read_csv("titanic.csv")
+# print(df)
 
-sns.displot(data=df, x="flipper_length_mm",
-            kind="kde", hue="species", col="species", height=3, aspect=1)
+# # sns.barplot(data=df, x="Pclass", y="Fare")
+# # sns.countplot(data=df, x="Pclass")
 
-sns.relplot(data=df, x="flipper_length_mm", y="bill_length_mm",
-            kind="scatter", hue="species", col="species", height=3, aspect=1)
+# print(df["Pclass"].value_counts())
+
+# # sns.barplot(data=df, x="Pclass", y="Fare", hue="Sex", estimator=np.mean)
+# # sns.boxplot(data=df, x="Pclass", y="Age")
+# # sns.violinplot(data=df, x="Pclass", y="Age")
+
+# data = np.random.rand(3, 3)
+# print(data)
+# sns.heatmap(data=data, annot=True, cmap="coolwarm")
+
+# Iris
+df = pd.read_csv("iris.csv")
+print(df)
+
+sns.pairplot(df, hue="Species")
 
 plt.show()
