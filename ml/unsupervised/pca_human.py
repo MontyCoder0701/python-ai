b3f93c0819804d279ec7d7affb497c16e356d72c
unknown <soojlee0106@naver.com>
2022-12-29 19:08:31 +0900

    pca for human dataset

--- ml/unsupervised/pca_human.py
+++ ml/unsupervised/pca_human.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 
 # 주성분 분석 (PCA with Iris dataset)
 df = pd.read_csv("ml/svm/train.csv")
@@ -32,29 +33,14 @@ x_train = scaler.transform(x_train)
 y_train = y_train.values
 
 # 차원 축소
-pca = PCA(n_components=5)
+pca = PCA(n_components=150)
 x_train = pca.fit_transform(x_train)
-print(x_train)
-
-# Visualize
-df = pd.DataFrame(x_train, columns=["x_1", "x_2", "x_3", "x_4", "x_5"])
 df["y_train"] = y_train
 
-sns.scatterplot(data=df, x="x_1", y="x_5", hue="y_train")
-plt.show()
-
 # 분류 작업 Random Forest
-df_x = df[["x_1", "x_2", "x_3", "x_4", "x_5"]]
+df_x = df
 df_y = df['y_train']
 
-clf = RandomForestClassifier(n_estimators=30, max_depth=3)
+clf = RandomForestClassifier()
 clf.fit(df_x, df_y)
 print(clf.score(df_x, df_y))
-print(clf.feature_importances_)
-
-sr = pd.Series(clf.feature_importances_, index=df_x.columns.values)
-sr = sr.sort_values(ascending=False)
-print(sr)
-
-sns.barplot(x=sr, y=sr.index)
-plt.show()
