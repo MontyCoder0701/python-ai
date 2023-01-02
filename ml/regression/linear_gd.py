a711a0a406032f175c55c1a4c06cff13dfcd15de
unknown <soojlee0106@naver.com>
2023-01-02 12:46:39 +0900

    Create class for linear regressor

--- ml/regression/linear_gd.py
+++ ml/regression/linear_gd.py
@@ -8,7 +8,7 @@ x_train = np.array([1., 2., 3., 4., 5., 6.])
 y_train = np.array([9., 12., 15., 18., 21., 24.])
 
 plt.scatter(x_train, y_train)
-plt.show()
+# plt.show()
 
 # 학습
 # 경사하강법 (gradient descent)
@@ -37,3 +37,44 @@ for i in range(epochs):
         print(f"Epoch: {i}, cost: {cost:10f}, w: {w:10f}, b: {b:10f}")
 
 # 3x+6으로 결과 나옴 (cost=0, w, b)
+
+# Class 생성하기
+y_pred = 3.0 * x_train + 6.0
+print(y_pred)
+
+
+class LinearRegressor:
+    def fit(self, x, y, epochs=100, lr=0.01):
+        self.w_ = 0.0
+        self.b_ = 0.0
+        self.n_ = len(x)
+        self.lr_ = lr
+
+        for i in range(epochs):
+            # 가설
+            h_hat = self.w_ * x_train + self.b_
+            # cost(mse)
+            cost = np.sum((y_train - h_hat)**2)/self.n_
+
+            # 미분
+            grad_w = np.sum(
+                (self.w_ * x_train + self.b_ - y_train)*2 * x_train)/self.n_
+            grad_b = np.sum((self.w_ * x_train + self.b_ - y_train)*2)/self.n_
+
+            self.w_ = self.w_ - lr * grad_w
+            self.b_ = self.b_ - lr * grad_b
+
+            if i % 200 == 0:
+                print(
+                    f"Epoch: {i}, cost: {cost:10f}, w: {self.w_:10f}, b: {self.b_:10f}")
+
+    def predict(self, x):
+        return self.w_ * x + self.b_
+
+
+lr = LinearRegressor()
+lr.fit(x_train, y_train, epochs=5000)
+
+x_test = np.array([10, 11])
+y_pred = lr.predict(x_test)
+print(y_pred)
