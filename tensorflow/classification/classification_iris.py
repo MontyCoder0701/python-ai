8ea632545269a413102d9b4bc8ac7682aa40f212
unknown <soojlee0106@naver.com>
2023-01-03 14:45:09 +0900

    Iris classification with tensorflow

---
+++ tensorflow/classification/classification_iris.py
@@ -0,0 +1,116 @@
+# Multi classification (Iris dataset)
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, mean_squared_error
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+
+def get_iris(mode=None):
+    iris = pd.read_csv('basics/iris.csv')
+    df = iris.drop(['Id'], axis=1).copy()
+
+    # Column 명칭
+    df.columns = ['sepal_length', 'sepal_width',
+                  'petal_length', 'petal_width', 'species']
+
+    # 이진분류
+    if (mode == 'bin'):
+        df = df.loc[df['species'] != 'Iris-virginica']
+
+    df['species'] = df['species'].map({
+        'Iris-setosa': 0,
+        'Iris-versicolor': 1,
+        'Iris-virginica': 2
+    })
+
+    # x, y 지정
+    x = df.drop(['species'], axis=1)
+    y = df['species']
+
+    # 섞고 test, train 나누기
+    return train_test_split(
+        x, y, test_size=0.2, random_state=2022)
+
+
+x_train, x_test, y_train, y_test = get_iris()
+
+# 표준화
+scaler = StandardScaler()
+x_train_s = scaler.fit_transform(x_train)
+y_train = y_train.values
+
+# 다중분류인 경우, y_train 값 변경 (one-hot encoding)
+y_train_oh = to_categorical(y_train)
+print(y_train_oh)
+
+# 모델
+model = keras.Sequential([
+    layers.Dense(units=64, activation="relu", input_shape=(4,)),
+    layers.Dense(units=32, activation="relu"),
+    # 이진 분류 모델 (sigmoid 출력층 - 0 혹은 1)/ 다중분류 (softmax)
+    layers.Dense(units=3, activation="softmax"),  # 다중분류 개수 만큼 최종 출력
+])
+
+model.summary()
+
+model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["acc"]
+)
+
+# 학습
+epochs = 200
+batch_size = 32
+
+history = model.fit(x_train_s, y_train_oh, epochs=epochs,
+                    batch_size=batch_size, validation_split=0.2, verbose=1)
+
+# 평가
+model.evaluate(x_train_s, y_train_oh)
+
+# 로그 확인
+print(history.history.keys())
+
+# matplotlib
+
+
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+
+    plt.figure(figsize=(16, 8))
+    plt.subplot(1, 2, 1)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.plot(hist['epoch'], hist['loss'], label='Train Loss')
+    plt.plot(hist['epoch'], hist['val_loss'], label='Val Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.plot(hist['epoch'], hist['acc'], label='Train Accuracy')
+    plt.plot(hist['epoch'], hist['val_acc'], label='Val Accuracy')
+    plt.legend()
+
+    plt.show()
+
+
+plot_history(history)
+
+# 예측
+x_test_s = scaler.transform(x_test)
+y_test = y_test.values
+y_test_oh = to_categorical(y_test)
+
+y_pred = model.predict(x_test_s)
+
+print(mean_squared_error(y_test_oh, y_pred))
