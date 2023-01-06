0e31d46bcc2e585c6900dd3ffb59b06583b81430
unknown <soojlee0106@naver.com>
2023-01-06 12:44:56 +0900

    Try out different layering methods

--- tensorflow/neural/classification/cnn_fashion.py
+++ tensorflow/neural/classification/cnn_fashion.py
@@ -1,4 +1,5 @@
 # CNN (BW Image classification- Fashion Dataset)
+from keras.models import Model
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, mean_squared_error
@@ -60,6 +61,78 @@ x_train = x_train.reshape(-1, 28, 28, 1)
 x_val = x_val.reshape(-1, 28, 28, 1)
 x_test = x_test.reshape(-1, 28, 28, 1)
 
+# 함수형 API (Sequential로 만들 수 없는 모델 만들 경우)
+
+
+def build_model():
+    inputs = layers.Input(shape=(28, 28, 1))
+    x1 = layers.Conv2D(filters=32, kernel_size=3,
+                       activation="relu")(inputs)
+    x2 = layers.MaxPool2D(pool_size=(2, 2))(x1)
+    x3 = layers.Conv2D(filters=32, kernel_size=3,
+                       activation="relu")(x2)
+    x4 = layers.MaxPool2D(pool_size=(2, 2))(x3)
+    x5 = layers.Flatten()(x4)
+    x6 = layers.Dense(units=32, activation="relu")(x5)
+    outputs = layers.Dense(units=10, activation="softmax")(x6)
+
+    model = Model(inputs=inputs, outputs=outputs)
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["acc"]
+    )
+    return model
+
+
+model = build_model()
+
+# 함수형 예제
+input_x = layers.Input(shape=(16,))
+x1 = layers.Dense(8, activation="relu")(input_x)
+x2 = layers.Dense(16, activation="relu")(x1)
+x3 = Model(inputs=input_x, outputs=x2)
+
+input_y = layers.Input(shape=(16,))
+x4 = layers.Dense(8, activation="relu")(input_y)
+x5 = layers.Dense(16, activation="relu")(x4)
+x6 = Model(inputs=input_y, outputs=x5)
+
+# 합치기
+combined = layers.concatenate([x3.output, x6.output])
+z1 = layers.Dense(2, activation="relu")(combined)
+z2 = layers.Dense(1)(z1)
+
+model = Model(inputs=[x3.input, x6.input], outputs=z2)
+
+# Sub classing (모델을 class로 만드는 것)
+
+
+# class MnistModel(Model):
+#     def __init__(self):
+#         super(MnistModel, self).__init__()
+#         self.conv1 = layers.Conv2D(64, 3, activation="relu")
+#         self.conv2 = layers.Conv2D(32, 3, activation="relu")
+#         self.maxpool = layers.MaxPool2D(2)
+#         self.flat = layers.Flatten()
+#         self.dense1 = layers.Dense(32, activation="relu")
+#         self.dense2 = layers.Dense(10, activation="softmax")
+
+#     def call(self, inputs):
+#         x = self.conv1(inputs)
+#         x = self.conv2(x)
+#         x = self.maxpool(x)
+#         x = self.flat(x)
+#         x = self.dense1(x)
+#         output = self.dense2(x)
+#         return output
+
+
+# model = MnistModel()
+
+
+# 일반 sequential
 model = keras.Sequential([
     layers.Conv2D(filters=32, kernel_size=3,
                   activation="relu", input_shape=(28, 28, 1)),
